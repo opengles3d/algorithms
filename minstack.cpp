e99c319b9992867d7fed2d774b5614f2a0edba87
changqing shao <shaocq@lenovo.com>
2020-05-07 11:05:00 +0800

    add min stack use two STL stacks

---
+++ minstack.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stack>
+
+class MinStack {
+public:
+    void push(int a) {
+        main_stack.push(a);
+        if(min_stack.empty() || a <= min_stack.top()) {
+            min_stack.push(a);
+        }
+    }
+
+    int pop() {
+        int at = main_stack.top();
+        int min = min_stack.top();
+        if(at == min) {
+            min_stack.pop();
+        }
+        main_stack.pop();
+        return at;
+    }
+    int getMin() {
+        int min = min_stack.top();
+        //min_stack.pop();
+        return min;
+    }
+private:
+    std::stack<int> main_stack;
+    std::stack<int> min_stack;
+};
+
+int main(int argc, char* argv[]) {
+    MinStack ms;
+    ms.push(4);
+    ms.push(5);
+    ms.push(6);
+    ms.push(3);
+    ms.push(7);
+
+    std::cout << "min is: " << ms.getMin() << std::endl;
+    std::cout << "Pop :" << ms.pop() << std::endl;
+
+    std::cout << "min is: " << ms.getMin() << std::endl;
+    std::cout << "Pop :" << ms.pop() << std::endl;
+
+    std::cout << "min is: " << ms.getMin() << std::endl;
+    std::cout << "Pop :" << ms.pop() << std::endl;
+
+    std::cout << "min is: " << ms.getMin() << std::endl;
+    std::cout << "Pop :" << ms.pop() << std::endl;
+
+    std::cout << "min is: " << ms.getMin() << std::endl;
+    std::cout << "Pop :" << ms.pop() << std::endl;
+
+    return 0;
+}
